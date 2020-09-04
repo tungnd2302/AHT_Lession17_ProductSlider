8849f26c6b630707ec95abb0e76c823e0b273e52
tungnd2302 <tungnd0318@gmail.com>
2020-09-04 23:55:53 +0700

    lession17_18

---
+++ Block/Catalog/Product/ProductSlider.php
@@ -0,0 +1,4 @@
+<?php
+namespace AHT\ProductSlider\Block\Catalog\Product;
+class ProductSlider extends \Magento\Catalog\Block\Product\ListProduct
+{ }
---
+++ Controller/Adminhtml/Catalog/Category/Widget/Chooser.php
@@ -0,0 +1,18 @@
+<?php
+namespace AHT\ProductSlider\Controller\Adminhtml\Catalog\Category\Widget;
+
+class Chooser extends \Magento\Catalog\Controller\Adminhtml\Category\Widget\Chooser {
+    
+    protected function _getCategoryTreeBlock() {
+   	 return $this->layoutFactory->create()->createBlock(
+        'AHT\ProductSlider\Block\Adminhtml\Catalog\Category\Widget\Chooser',
+            '',
+            [
+                'data' => [
+                    'id' => $this->getRequest()->getParam('uniq_id'),
+                    'use_massaction' => $this->getRequest()->getParam('use_massaction', false),
+                ]
+            ]
+        );
+    }
+}
